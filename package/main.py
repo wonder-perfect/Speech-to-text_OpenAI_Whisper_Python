532b74152b1ff33f5eab490794e14c3fd2943615
perfect-everything <3483711648@qq.com>
2023-08-25 23:01:18 +0800

    modified:   .github/workflows/macos_packaging.yml 	modified:   package/main.py

--- package/main.py
+++ package/main.py
@@ -6,8 +6,24 @@ import subprocess
 import configparser
 import sys
 import json
+import shutil
 
 WARN_cnt = 0
+
+
+def delete_all_files_in_directory(directory_path):
+    for filename in os.listdir(directory_path):
+        file_path = os.path.join(directory_path, filename)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+
+
+def compare_files(file1_path, file2_path):
+    with open(file1_path, "rb") as file1, open(file2_path, "rb") as file2:
+        content1 = file1.read()
+        content2 = file2.read()
+
+    return content1 == content2
 
 
 def whisper_translate(file, model, prompt, response_format, temperature):
@@ -41,6 +57,25 @@ else:
     base_path = os.path.dirname(os.path.abspath(__file__))
 
 config_path = os.path.join(base_path, 'config.ini')
+tmp_output_dir = os.path.join(base_path, "tmp")
+tmp_output_path = os.path.join(tmp_output_dir, "ffmpeg_tmp.wav")
+preprocess_dir = os.path.join(base_path, "preprocess_audio")
+loop_trigger_path = os.path.join(preprocess_dir, "loop_trigger")
+
+if os.path.exists(preprocess_dir):
+    delete_all_files_in_directory(preprocess_dir)
+else:
+    os.mkdir(preprocess_dir)
+
+with open(loop_trigger_path, "w") as loop_trigger:
+    loop_trigger.write("This file is for triggering the loop. Do not move!")
+
+if os.path.exists(tmp_output_path):
+    os.remove(tmp_output_path)
+    print(f"Exising tmp file {tmp_output_path} found, deleting...")
+elif not os.path.exists(tmp_output_dir):
+    os.mkdir(tmp_output_dir)
+    print(f"Making new tmp directory {tmp_output_dir} ...\n")
 
 if os.path.exists(config_path):
     print("\nconfig.ini file found. Loading config...")
@@ -142,9 +177,7 @@ else:
     print(f"\n\033[93mWARN: Unsupported model. Model set to {model}.\033[0m")
 
 print(f"\n\033[94mLIMITATION: 25MB\nINPUT FILE TYPES SUPPORTED: {supported_file_types}\n\
-OUTPUT FORMAT SUPPORTED: {supported_output_formats}\033[0m\n\n\
-\033[93mPlease remove the same files in different extensions!\n\
-The program will auto convert supported files in the working directory into WAV files!!!\033[0m")
+OUTPUT FORMAT SUPPORTED: {supported_output_formats}\033[0m\n")
 
 work_dir = input("Enter the directory your audio file located\n\
 (Current path will be used as working directory if nothing is entered): ")
@@ -165,45 +198,63 @@ else:
     print(f"Transcribing {audio_filename}\n\n")
     filenames = [audio_filename]
 
+preprocess_cnt = 0
+transcribe_cnt = 0
+
 for filename in filenames:
-    path = os.path.join(work_dir, filename)
+    isSame = False
 
-    if os.path.splitext(filename)[1].lower() != ".wav":
-        print(f"Converting {filename} to WAV...\n")
-        output_wav_filename = os.path.splitext(filename)[0] + ".wav"
-        if output_wav_filename in filenames:
-            print(f"\033[93mWARN: WAV file {output_wav_filename} already exists, skipping...\n\
-WARN: This program only checks if the filename is the same. Please rename the file if the files are different!\033[0m\n")
-            WARN_cnt += 1
-            continue
-        output_wav_path = os.path.join(work_dir, output_wav_filename)
-        subprocess.run(["ffmpeg", "-i", path, output_wav_path])
-        print(f"Converted {filename} to {output_wav_filename}\n")
-        path = output_wav_path
+    input_file_path = os.path.join(work_dir, filename)
+    preprocess_filename = str(preprocess_cnt) + ".wav"
+    preprocess_path = os.path.join(preprocess_dir, preprocess_filename)
+    print(f"Converting {filename} to WAV...\n")
 
-    file_size_mb = os.path.getsize(path) / (1024 * 1024)
+    subprocess.run(["ffmpeg", "-loglevel", "error", "-i", input_file_path, "-ac", "1", "-ar", "16000", tmp_output_path])
+    print(f"Converted {filename} to ffmpeg_tmp.wav\n")
+
+    preprocess_files = os.listdir(preprocess_dir)
+    for preprocess_file in preprocess_files:
+        preprocess_file_path = os.path.join(preprocess_dir, preprocess_file)
+        if os.path.getsize(tmp_output_path) == os.path.getsize(preprocess_file_path):
+            os.remove(tmp_output_path)
+            isSame = True
+            break
+    
+    if isSame:
+        isSame = False
+        continue
+
+    shutil.move(tmp_output_path, preprocess_path)
+    preprocess_cnt += 1
+
+    if transcribe_cnt >= preprocess_cnt:
+        continue
+
+    file_size_mb = os.path.getsize(preprocess_path) / (1024 * 1024)
     if file_size_mb > MAX_FILE_SIZE_MB:
         print(f"\033[93mWARN: File {filename} is larger than {MAX_FILE_SIZE_MB} and cannot be transcribed!\033[0m\n")
         WARN_cnt += 1
+        transcribe_cnt += 1
         continue
 
-    with open(path, "rb") as audio_file:
+    output_path = os.path.join(work_dir, os.path.splitext(filename)[0] + output_file_ext)
+
+    with open(preprocess_path, "rb") as audio_file:
         if translation == "True":
             transcript = whisper_translate(audio_file, model, prompt, response_format, temperature)
-            print(f"Output from {path} (Translation: {translation}, Model: {model}, Prompt: {prompt},\
-Response_format: {response_format}, Temperature: {temperature}):")
+            print(f"Output from {input_file_path} (Translation: {translation}, Model: {model}, Prompt: {prompt},\
+Response_format: {response_format}, Temperature: {temperature}):\n")
         else:
             transcript = whisper_transcribe(audio_file, model, prompt, response_format, temperature, language)
-            print(f"Output from {path} (Translation: {translation}, Model: {model}, Prompt: {prompt},\
-Response_format: {response_format}, Temperature: {temperature}, Language: {language}):")
+            print(f"Output from {input_file_path} (Translation: {translation}, Model: {model}, Prompt: {prompt},\
+Response_format: {response_format}, Temperature: {temperature}, Language: {language}):\n")
     
     print(transcript)
-    
-    output_path = os.path.splitext(path)[0] + output_file_ext
-    
+    transcribe_cnt += 1
+
     with open(output_path, "w", encoding="utf-8") as output_file:
         if response_format == "json" or response_format == "verbose_json":
-            json.dump(transcript, output_file, intend=4)
+            json.dump(transcript, output_file, indent=4)
         else:
             output_file.write(transcript)
 
@@ -213,4 +264,5 @@ if WARN_cnt == 0:
     print(f"\nPROGRAM EXECUTED SUCCESSFULLY WITH {WARN_cnt} WARNINGS")
 else:
     print(f"\033[93m\n\nPROGRAM EXECUTED SUCCESSFULLY WITH {WARN_cnt} WARNINGS\033[0m")
+    
 os.system("pause")
