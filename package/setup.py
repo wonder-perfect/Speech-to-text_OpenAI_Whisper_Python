db92441ed3020a6d0104cc086fb8c401d759dab6
perfect-everything <3483711648@qq.com>
2023-08-26 21:16:40 +0800

    modified:   .github/workflows/macos_packaging.yml 	renamed:    package/whisper.ico -> package/icons/whisper.icns 	new file:   package/icons/whisper.ico 	modified:   package/setup.py

--- package/setup.py
+++ package/setup.py
@@ -3,12 +3,15 @@ from setuptools import setup
 APP = ['main.py']
 DATAFILES = ['config.ini', 'ffmpeg']
 OPTIONS = {
-    'argv_emulation': True
+    'argv_emulation': True,
+    'arch': 'universal2',
+    'iconfile': './icon/whisper.icns'
 }
 
 setup(
     app = APP,
+    name = 'pySTT_Whisper_API',
     data_files = DATAFILES,
     options = {'py2app': OPTIONS},
-    setup_requires = ['py2app'],
+    setup_requires = ['py2app']
 )
