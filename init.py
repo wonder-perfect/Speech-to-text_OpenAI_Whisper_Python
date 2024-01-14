1c1eeb1eab1cf4b6db92d5830685e68528f5fbdc
perfect-everything <3483711648@qq.com>
2024-01-14 11:43:51 +0800

    Refactor config file handling and change config format from ini to json

--- init.py
+++ init.py
@@ -1,16 +1,36 @@
 # -*- coding: UTF-8 -*-
 
-import configparser
+import json
 import os
+
+DEFAULT_CONFIG = {
+    "general": {
+        "openai_api_key": "",
+        "temperature": "0"
+    },
+    "audio": {
+        "translation": "False",
+        "model": "whisper-1",
+        "prompt": "",
+        "response_format": "text",
+        "language": "auto"
+    },
+    "punctuation": {
+        "model": "gpt-3.5-turbo",
+        "punctuation": "False"
+    }
+}
 
 # Generate default config file if it's not exsist
 def generate_config(config_dir, config_path):
+    if not os.path.exists(config_dir):
+        os.makedirs(config_dir)
     delete_all_files_in_directory(config_dir)
     openai_api_key = input("Enter your OpenAI API Key: ")
-    config_ini = f'# -*- coding: UTF-8 -*-\n\n[Settings]\nopenai_api_key = {openai_api_key}\ntranslation = False\nmodel = whisper-1\n\
-prompt = \nresponse_format = text\ntemperature = 0\nlanguage = auto\n\n[GPT]\nmodel = gpt-3.5-turbo\npunctuation = False\n'
+    config = DEFAULT_CONFIG
+    config["general"]["openai_api_key"] = openai_api_key
     with open(config_path, "w", encoding='utf-8') as config_file:
-        config_file.write(config_ini)
+        json.dump(config, config_file, indent=4)
 
 # Delete all the files in the directory
 def delete_all_files_in_directory(directory_path):
@@ -20,7 +40,7 @@ def delete_all_files_in_directory(directory_path):
             os.remove(file_path)
 
 # Initialize working environment
-def load_config(config_dir, config_path, tmp_output_dir, tmp_output_path, preprocess_dir, loop_trigger_path):
+def load_config(config_dir, config_path, tmp_output_dir, tmp_output_path, preprocess_dir, loop_trigger_path, supported_output_formats, supported_models):
 
     # Create tmp files and dirs
     if os.path.exists(preprocess_dir):
@@ -39,123 +59,32 @@ def load_config(config_dir, config_path, tmp_output_dir, tmp_output_path, prepro
         print(f"Making new tmp directory {tmp_output_dir} ...\n")
 
     if os.path.exists(config_path):
-        print("\nconfig.ini file found. Loading config...")
+        config = json.load(open(config_path, encoding='utf-8'))
+        print("\nconfig.json file found. Loading config...")
     else:
-        print("\033[93mWARN: config.ini file not found!\nGenerating config file...\033[0m\n")
         generate_config(config_dir, config_path)
+        print("\033[93m[WARN]: config.json file not found!\nGenerating config file...\033[0m\n")
 
-    # Read the config file
-    config = configparser.ConfigParser()
-
-    with open(config_path, encoding='utf-8') as config_file:
-        config.read_file(config_file)
-
-    try:
-        openai_api_key = config.get("Settings", "openai_api_key")
-    except configparser.NoSectionError:
-        generate_config(config_dir, config_path)
-        print('\n\033[93mWARN: Section "Settings" not found in config.ini\n\
-Generating default config file...\033[0m')
-    except configparser.NoOptionError:
-        generate_config(config_dir, config_path)
-        print('\n\033[93mWARN: Option "openai_api_key" not found in config.ini\n\
-Generating default config file...\033[0m')
-
-    try:
-        translation = config.get("Settings", "translation")
-    except configparser.NoOptionError:
-        translation = False
-        print(f'\n\033[93mWARN: Option "translation" in section "Settings" not found in config.ini\n\
-Translation set to {translation}.\033[0m')
-
-    try:
-        model = config.get("Settings", "model")
-    except configparser.NoOptionError:
-        model = "whisper-1"
-        print(f'\n\033[93mWARN: Option "model" in section "Settings" not found in config.ini\n\
-Model set to {model}.\033[0m')
-
-    try:
-        prompt = config.get("Settings", "prompt")
-    except configparser.NoOptionError:
-        prompt = ""
-        print(f'\n\033[93mWARN: Option "prompt" in section "Settings" not found in config.ini\n\
-Prompt set to {prompt}.\033[0m')
-
-    try:
-        response_format = config.get("Settings", "response_format")
-    except configparser.NoOptionError:
-        response_format = "text"
-        print(f'\n\033[93mWARN: Option "response_format" in section "Settings" not found in config.ini\n\
-Response_format set to {response_format}.\033[0m')
-
-    try:
-        temperature = config.get("Settings", "temperature")
-    except configparser.NoOptionError:
-        temperature = 0
-        print(f'\n\033[93mWARN: Option "temperature" in section "Settings" not found in config.ini\n\
-Temperature set to {temperature}.\033[0m')
-
-    try:
-        language = config.get("Settings", "language")
-    except configparser.NoOptionError:
-        language = "auto"
-        print(f'\n\033[93mWARN: Option "language" in section "Settings" not found in config.ini\n\
-Language set to {language}.\033[0m')
-
-
-    try:
-        gpt_model = config.get("GPT", "model")
-    except configparser.NoOptionError:
-        gpt_model = "gpt-3.5-turbo"
-        print(f'\n\033[93mWARN: Option "model" in section "GPT" not found in config.ini\n\
-GPT_Model set to {gpt_model}.\033[0m')
-    
-    try:
-        punctuation = config.get("GPT", "punctuation")
-    except configparser.NoOptionError:
-        punctuation = "True"
-        print(f'\n\033[93mWARN: Option "punctuation" in section "GPT" not found in config.ini\n\
-Puncuation set to {punctuation}.\033[0m')
-        
-    config_values = {
-        "openai_api_key": openai_api_key,
-        "translation": translation,
-        "model": model,
-        "prompt": prompt,
-        "response_format": response_format,
-        "temperature": temperature,
-        "language": language,
-        "gpt_model": gpt_model,
-        "punctuation": punctuation
-    }
+    config['audio']['response_format'], config['audio']['model'] = is_config_values_valid(
+        config['audio']['response_format'], 
+        supported_output_formats, 
+        config['audio']['model'], 
+        supported_models
+    )
 
     print("\nConfig loaded successfully.")
     
-    return config_values
+    return config
 
 # Check if config values are valid
 def is_config_values_valid(response_format, supported_output_formats, model, supported_models):
 
-    output_ext_mapping = {
-        "json": ".json",
-        "text": ".txt",
-        "srt": ".srt",
-        "verbose_json": ".verbose_json",
-        "vtt": "vtt",
-    }
-
-    if response_format in supported_output_formats:
-        output_file_ext = output_ext_mapping[response_format]
-    else:
+    if response_format not in supported_output_formats:
         response_format = supported_output_formats[1]
-        output_file_ext = output_ext_mapping[response_format]
-        print(f"\n\033[93mWARN: Unsupported response_format. Response_format set to {response_format}.\033[0m")
+        print(f"\n\033[93m[WARN]: Unsupported response_format. Response_format set to {response_format}.\033[0m")
 
-    if model in supported_models:
-        model
-    else:
+    if model not in supported_models:
         model = supported_models[0]
-        print(f"\n\033[93mWARN: Unsupported model. Model set to {model}.\033[0m")
+        print(f"\n\033[93m[WARN]: Unsupported model. Model set to {model}.\033[0m")
     
-    return output_file_ext, model
+    return response_format, model
--- main.py
+++ main.py
@@ -10,6 +10,16 @@ import init
 
 client = OpenAI()
 
+def map_output_file_ext(response_format):
+    output_file_ext_mapping = {
+        "json": ".json",
+        "text": ".txt",
+        "srt": ".srt",
+        "verbose_json": ".verbose_json",
+        "vtt": ".vtt"
+    }
+    return output_file_ext_mapping[response_format]
+
 def output(output_path, transcript, response_format):
     with open(output_path, "w", encoding="utf-8") as output_file:
         if response_format == "json" or response_format == "verbose_json":
@@ -25,28 +35,28 @@ def compare_files(file1_path, file2_path):
     return content1 == content2
 
 def whisper_translate(file, model, prompt, response_format, temperature):
-    if prompt == "":
-        return client.audio.translations.create(file=file, model=model, response_format=response_format, \
-                                      temperature=temperature)
-    else:
-        return client.audio.translations.create(file=file, model=model, prompt=prompt, response_format=response_format, \
-                                      temperature=temperature)
+    params = {
+        'file': file,
+        'model': model,
+        'response_format': response_format,
+        'temperature': temperature
+    }
+    if prompt != "":
+        params['prompt'] = prompt
+    return client.audio.translations.create(**params)
 
 def whisper_transcribe(file, model, prompt, response_format, temperature, language):
-    if prompt == "":
-        if language == "auto":
-            return client.audio.transcriptions.create(file=file, model=model, response_format=response_format, \
-                                          temperature=temperature)
-        else:
-            return client.audio.transcriptions.create(file=file, model=model, response_format=response_format, \
-                                          temperature=temperature, language=language)
-    else:
-        if language == "auto":
-            return client.audio.transcriptions.create(file=file, model=model, prompt=prompt, response_format=response_format, \
-                                          temperature=temperature)
-        else:
-            return client.audio.transcriptions.create(file=file, model=model, prompt=prompt, response_format=response_format, \
-                                          temperature=temperature, language=language)
+    params = {
+        'file': file,
+        'model': model,
+        'response_format': response_format,
+        'temperature': temperature
+    }
+    if prompt != "":
+        params['prompt'] = prompt
+    if language != "auto":
+        params['language'] = language
+    return client.audio.transcriptions.create(**params)
 
 def gpt_punctuation(transcript, gpt_model):
     prompt = "Add the punctuation for the following text.\n" + transcript
@@ -87,8 +97,22 @@ OUTPUT FORMAT SUPPORTED: {supported_output_formats}\033[0m\n")
 
     return filenames, work_dir
 
-def process_audios(filenames, work_dir, preprocess_dir, tmp_output_path, output_file_ext, \
-                   translation, model, prompt, response_format, temperature, language, gpt_model, punctuation):
+def process_audios(
+        filenames: str,
+        work_dir: str,
+        preprocess_dir: str,
+        tmp_output_path: str,
+        output_file_ext: str,
+        translation: str,
+        model: str,
+        prompt: str,
+        response_format: str,
+        temperature: str,
+        language: str,
+        gpt_model: str,
+        punctuation: str
+    ) -> None:
+
     MAX_FILE_SIZE_MB = 25
     preprocess_cnt = 0
     transcribe_cnt = 0
@@ -113,7 +137,7 @@ def process_audios(filenames, work_dir, preprocess_dir, tmp_output_path, output_
                 isSame = True
                 print(f"Files {tmp_output_path} and {preprocess_file_path} are same. Skipping...")
                 break
-    
+
         if isSame:
             isSame = False
             continue
@@ -129,22 +153,28 @@ def process_audios(filenames, work_dir, preprocess_dir, tmp_output_path, output_
             print(f"\033[93mWARN: File {filename} is larger than {MAX_FILE_SIZE_MB} and cannot be transcribed!\033[0m\n")
             transcribe_cnt += 1
             continue
-    
+
         output_dir = os.path.join(work_dir, "output")
         if not os.path.exists(output_dir):
             os.mkdir(output_dir)
         transcript_output_path = os.path.join(output_dir, os.path.splitext(filename)[0] + "_transcript" + output_file_ext)
 
         with open(preprocess_path, "rb") as audio_file:
+            params = {
+                'file': audio_file,
+                'model': model,
+                'prompt': prompt,
+                'response_format': response_format,
+                'temperature': temperature
+            }
             if translation == "True":
-                transcript = whisper_translate(audio_file, model, prompt, response_format, temperature)
-                print(f"Output from {input_file_path} (Translation: {translation}, Model: {model}, Prompt: {prompt},\
-Response_format: {response_format}, Temperature: {temperature}):\n")
+                transcript = whisper_translate(**params)
             else:
-                transcript = whisper_transcribe(audio_file, model, prompt, response_format, temperature, language)
-                print(f"Output from {input_file_path} (Translation: {translation}, Model: {model}, Prompt: {prompt},\
-Response_format: {response_format}, Temperature: {temperature}, Language: {language}):\n")
-    
+                params['language'] = language
+                transcript = whisper_transcribe(**params)
+            print(f"Output from {input_file_path} (Translation: {translation}, Model: {model}, Prompt: {prompt},\
+                    Response_format: {response_format}, Temperature: {temperature}, Language: {language}):\n")
+
         print(transcript)
         transcribe_cnt += 1
 
@@ -179,31 +209,55 @@ def main():
     else:
         base_path = os.path.dirname(os.path.abspath(__file__))
 
-    config_dir = os.path.join(base_path, "./config")
-    config_path = os.path.join(config_dir, "config.ini")
+    config_dir = os.path.join(base_path, "config\\")
+    config_path = os.path.join(config_dir, "config.json")
     tmp_output_dir = os.path.join(base_path, "tmp")
     tmp_output_path = os.path.join(tmp_output_dir, "ffmpeg_tmp.wav")
     preprocess_dir = os.path.join(base_path, "preprocess_audio")
     loop_trigger_path = os.path.join(preprocess_dir, "loop_trigger")
 
-    config_values = init.load_config(config_dir, config_path, tmp_output_dir, tmp_output_path, preprocess_dir, loop_trigger_path)
+    config_values = init.load_config(
+        config_dir,
+        config_path,
+        tmp_output_dir,
+        tmp_output_path,
+        preprocess_dir,
+        loop_trigger_path,
+        supported_output_formats,
+        supported_models
+    )
 
-    client.api_key = os.environ.get["OPENAI_API_KEY"] or config_values["openai_api_key"]
-    translation = config_values["translation"]
-    model = config_values["model"]
-    prompt = config_values["prompt"]
-    response_format = config_values["response_format"]
-    temperature = config_values["temperature"]
-    language = config_values["language"]
-    gpt_model = config_values["gpt_model"]
-    punctuation = config_values["punctuation"]
+    client.api_key =  config_values['general']['openai_api_key'] or os.environ.get('OPENAI_API_KEY')
+    translation = config_values['audio']['translation']
+    model = config_values['audio']['model']
+    prompt = config_values['audio']['prompt']
+    response_format = config_values['audio']['response_format']
+    temperature = config_values['general']['temperature']
+    language = config_values['audio']['language']
+    gpt_model = config_values['punctuation']['model']
+    punctuation = config_values['punctuation']['punctuation']
 
-    output_file_ext, model = init.is_config_values_valid(response_format, supported_output_formats, model, supported_models)
+    print(f"[DEBUG]: OpenAI API Key: {client.api_key}\n") # DEBUG
+
+    output_file_ext = map_output_file_ext(response_format)
 
     filenames, work_dir = user_interact(supported_file_types, supported_output_formats)
 
-    process_audios(filenames, work_dir, preprocess_dir, tmp_output_path, output_file_ext, \
-                   translation, model, prompt, response_format, temperature, language, gpt_model, punctuation)
+    process_audios(
+        filenames,
+        work_dir,
+        preprocess_dir,
+        tmp_output_path,
+        output_file_ext,
+        translation,
+        model,
+        prompt,
+        response_format,
+        temperature,
+        language,
+        gpt_model,
+        punctuation
+    )
 
     cleanup(preprocess_dir)
 
